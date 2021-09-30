331e6561d73c8fb8b492c3d9a44441b6927f307c
kodarit11 <taavi.makela@gmail.com>
2021-09-30 19:27:27 +0300

    pelaaja aloitettu

---
+++ 2021Syksy_To1830_Multiplayer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    public float moveSpeed = 8f;
+    public float runSpeed = 14f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    [SerializeField] private bool isGrounded;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
