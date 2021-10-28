ec6c7a2d5877137d72f51fc49698dbeda40fb84c
kodarit11 <taavi.makela@gmail.com>
2021-10-28 19:26:51 +0300

    mouse look valmis

--- 2021Syksy_To1830_Multiplayer/Assets/Scripts/MouseLook.cs
+++ 2021Syksy_To1830_Multiplayer/Assets/Scripts/MouseLook.cs
@@ -15,12 +15,20 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = mode;
+        playerBody = gameObject.transform.parent;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mouseX = Input.GetAxis("Mouse X") *mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") *mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation,minXAngle,maxAngle);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
